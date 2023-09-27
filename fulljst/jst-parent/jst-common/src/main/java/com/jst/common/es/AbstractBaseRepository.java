3cd5ee29de4e7989e877f29e3e5fd2a26b05aa4f
jiashitong <shitongjia0909@163.com>
2023-09-27 19:14:59 +0800

    0927

--- fulljst/jst-parent/jst-common/src/main/java/com/jst/common/es/AbstractBaseRepository.java
+++ fulljst/jst-parent/jst-common/src/main/java/com/jst/common/es/AbstractBaseRepository.java
@@ -2,14 +2,18 @@ package com.jst.common.es;
 
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
+import com.jst.common.dto.EsDcResult;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.http.util.EntityUtils;
 import org.elasticsearch.client.Request;
 import org.elasticsearch.client.Response;
 import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.common.StopWatch;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.Executor;
 
 public abstract class AbstractBaseRepository implements BaseRepository{
@@ -41,11 +45,26 @@ public abstract class AbstractBaseRepository implements BaseRepository{
 
     private String getIdcNameByEsClient(RestHighLevelClient esClient){
         try{
-            //return this.getSelector().getIdcNameByEsClient();
+            //return this.getSelector().getIdcNameByEsClient(esClient);
         }catch (Exception e){
             logger.error("getIdcNameByEsClient");
+            return null;
         }
     }
+
+    public EsDcResult<JSONObject> query(String sql){
+        StopWatch stopWatch = new StopWatch();
+        stopWatch.start();
+        Map<String, String> idcStatusMap = new HashMap<>();
+        Map<String, JSONObject> resultObject = new HashMap<>();
+        try{
+
+        }catch (Exception e){
+
+        }
+
+    }
+
 
 
 
