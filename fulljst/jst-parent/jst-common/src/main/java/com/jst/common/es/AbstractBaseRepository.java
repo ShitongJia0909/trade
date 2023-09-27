495ff4fddcc2d2f0ac37fee643abe518f45fbd62
jiashitong <shitongjia0909@163.com>
2023-09-27 18:44:46 +0800

    0927

--- fulljst/jst-parent/jst-common/src/main/java/com/jst/common/es/AbstractBaseRepository.java
+++ fulljst/jst-parent/jst-common/src/main/java/com/jst/common/es/AbstractBaseRepository.java
@@ -1,13 +1,53 @@
 package com.jst.common.es;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.http.util.EntityUtils;
+import org.elasticsearch.client.Request;
+import org.elasticsearch.client.Response;
+import org.elasticsearch.client.RestHighLevelClient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.concurrent.Executor;
+
 public abstract class AbstractBaseRepository implements BaseRepository{
 
-    Logger logger = LoggerFactory.getLogger(AbstractBaseRepository.class);
+    private static final Logger logger = LoggerFactory.getLogger(AbstractBaseRepository.class);
 
-    public abstract EsSourceSelector
+    public abstract EsSourceSelector getSelector();
+
+    public abstract Executor getExecutor();
+
+    @Override
+    public JSONObject query(RestHighLevelClient esClient, String sql){
+        String requestBody = "{\"query\":\"" + sql + "\",\"fetch_size\":10000}";
+        String strResponse = "";
+
+        String idcName = getIdcNameByEsClient(esClient);
+        try{
+            Request scriptRequest = new Request("POST", "_sql?format=json");
+            scriptRequest.setJsonEntity(requestBody);
+            Response scriptResponse = esClient.getLowLevelClient().performRequest(scriptRequest);
+            strResponse = EntityUtils.toString(scriptResponse.getEntity(), "UTF-8");
+            logger.info("queryFromEs idc:{}, requestBody:{}, response:{}",idcName,requestBody, StringUtils.left(strResponse, 1000)");
+        }catch (Exception e){
+            logger.error("queryFromEs idc:{}, requestBody:{}, response:{}",idcName,requestBody, StringUtils.left(strResponse, 1000), e);
+            throw new RuntimeException(e);
+        }
+        return JSON.parseObject(strResponse);
+    }
+
+    private String getIdcNameByEsClient(RestHighLevelClient esClient){
+        try{
+            //return this.getSelector().getIdcNameByEsClient();
+        }catch (Exception e){
+            logger.error("getIdcNameByEsClient");
+        }
+    }
+
+
 
 
 }
